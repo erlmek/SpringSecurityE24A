dc39e7abfba9dfecbe4dc8005fc490f250a7c5da
erlmek <erlm@ek.dk>
2025-10-31 15:27:16 +0100

    AuthenticationProvider

--- src/main/java/org/example/springsecuritye24a/config/ProjectSecurityConfig.java
+++ src/main/java/org/example/springsecuritye24a/config/ProjectSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,9 +15,10 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrfConfig -> csrfConfig.disable());
         http.authorizeHttpRequests((requests) -> {
             requests.requestMatchers("/myAccount","/myBalance").authenticated()
-                    .requestMatchers("/contact").permitAll();
+                    .requestMatchers("/contact","/register").permitAll();
         });
         //http.formLogin(flc -> flc.disable() ); //bruger browsers default login skærm
         http.formLogin(Customizer.withDefaults() );
@@ -21,4 +26,17 @@ public class ProjectSecurityConfig {
         var obj = http.build();
         return obj;
     }
+
+    public PasswordEncoder passwordEncoderNone() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        //var obj = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        //return obj;
+        return new BCryptPasswordEncoder(20);
+    }
+
+
 }
